13bb69b88830a05616c654a508927109c75f64d1
SangeethaSharvani <pendotasangeetha02@gmail.com>
2026-02-28 00:11:21 +0530

    Use dynamic PORT for production

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 // 🔥 Mount identify route
 app.use("/identify", identifyRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
